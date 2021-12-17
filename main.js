216089ebba76b8f1bd34b6d0983f76c5d97def0a
erger565 <94860476+erger565@users.noreply.github.com>
2021-12-16 19:47:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function(error) {
-    console.log('Service worker registration failed:', error);
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
-} else {
-  console.log('Service workers are not supported.');
 }
